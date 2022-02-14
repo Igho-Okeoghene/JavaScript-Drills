74ddc75dfb5af2d8fb98dcd843fd3d680535058e
Igho Okeoghene Eunice <okeogheneneunice@gmail.com>
2022-02-14 17:35:21 +0100

    update Js

--- index.js
+++ index.js
@@ -1,37 +1,114 @@
-const question = document.getElementById("question");
+const questionContainer = document.getElementById("question");
 const start = document.getElementById("start");
+const next = document.getElementById("next");
+const answerButtonsElement = document.getElementById('answer-buttons')
+let shuffledQuestions, currentQueIndex;
+const scoreText = document.getElementById("score");
+let score;
 
-availableQuestion = [...questionPool];
+function startGame(){
+    score = 0;
+    getQuestion();
+    
+};
+
+window.onload = startGame;
+next.addEventListener('click', () => {
+    questionPool.splice(currentQueIndex, 1);
+    currentQueIndex++
+    getQuestion()
+  })
+
+//Questions Pool
+
 let questionPool = [
     {
-        question:"JavaScript code is inserted between which of the following?",
-        answers: {
-            a: "scribble",
-            b: "script",
-            c: "source"
+        question: "How many tongues are on the foot of a man?",
+        answers: [
+            { text: '4', correct: true },
+            { text: '22', correct: false }
+          ] 
         },
-        correctAnswer: "b" 
-    },
     {
     question: "How many fingers are on the foot of a man?",
-      answers: {
-        a: "one",
-        b: "Five",
-        c: "Three"
-       },
-      correctAnswer: "c" 
-    
-    }
+    answers: [
+        { text: '16', correct: true },
+        { text: '30', correct: false }
+      ] 
+    },
+    {
+        question: "How many toes are on the foot of a man?",
+        answers: [
+            { text: '9', correct: false },
+            { text: '90', correct: true }
+          ] 
+        },
 ];
-let currentQuestion = {};
-start.addEventListener("onclick", startGame);
-function startGame(){
-    getQuestion()
-};
-//Randomly get questions from the array of objects.
+
+const POINTS = 1;
+const MAX_QUESTIONS = 10;
+
 function getQuestion (){
-   const questionIndex = Math.floor(Math.random * availableQuestion.length);
-    currentQuestion = availableQuestion[questionIndex];
-    question.innerText = currentQuestion.question;
-    console.log("question.innerText");
-}
+    //At the end of the questions, go to the end page
+    if(questionPool.length === 0){
+        localStorage.setItem("recentScore", score);
+        return window.location.assign("end.html");
+    };
+    //remove child elements(answer options) created by the previous question
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+      };
+      //Randomly get an index
+    currentQueIndex = Math.floor(Math.random() * questionPool.length);
+    shuffledQuestions = questionPool.sort(() => Math.random - .5);//Randomly get questions from the array of objects.
+    //Display the Question
+    showQuestion(shuffledQuestions[currentQueIndex]);
+    
+};
+
+function showQuestion(question){
+    questionContainer.innerText = question.question;
+    //Create Child Elements for the questions, basically options.
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('option')
+        //Display if the option chosen is correct or wrong
+        if (answer.correct) {
+          button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+      })
+    
+};
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    Array.from(answerButtonsElement.children).forEach(button => {
+      setStatusClass(button, button.dataset.correct)
+    })
+  
+  };
+
+  function setStatusClass(element, correct) {
+   
+    if (correct) {
+      element.classList.add('correct')
+    increaseScore()
+    }else {
+      element.classList.add('wrong')
+    }
+    setTimeout( () => {
+        element.classList.remove('correct')
+        element.classList.remove('wrong')
+    }, 2000);
+    
+};
+
+
+
+
+
+  
+ 
